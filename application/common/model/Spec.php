29433b5327179c945bdfac94c51f2c260321fe81
思维空间 <dnyz520@126.com>
2019-12-10 16:54:35 +0800

    up

--- application/common/model/Spec.php
+++ application/common/model/Spec.php
@@ -309,10 +309,7 @@ class Spec extends CareyShop
         $result = self::all(function ($query) {
             $with = ['getGoodsType'];
             $with['specItem'] = function ($query) {
-                $query
-                    ->field('spec_id,spec_item_id,item_name')
-                    ->where(['is_contact' => ['eq', 1]])
-                    ->order(['sort' => 'asc']);
+                $query->where(['is_contact' => ['eq', 1]])->order(['sort' => 'asc']);
             };
 
             $map['spec.goods_type_id'] = ['neq', 0];
