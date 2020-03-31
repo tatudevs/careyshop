415295f809c6ec7aea78837126c971e0fe629272
思维空间 <dnyz520@126.com>
2020-03-31 16:16:48 +0800

    新增"条形码"模块

--- application/common/service/Barcode.php
+++ application/common/service/Barcode.php
@@ -10,7 +10,6 @@
 
 namespace app\common\service;
 
-use CodeItNow\BarcodeBundle\Utils\BarcodeGenerator;
 use think\Url;
 use think\Loader;
 
@@ -45,64 +44,66 @@ class Barcode extends CareyShop
         empty($data['generate']) && $data['generate'] = 'image';
         empty($data['suffix']) && $data['suffix'] = 'png';
 
-        switch ($data['type']) {
-            case 'codabar':
-                $type = BarcodeGenerator::Codabar;
-                break;
-            case 'code11':
-                $type = BarcodeGenerator::Code11;
-                break;
-            case 'code39':
-                $type = BarcodeGenerator::Code39;
-                break;
-            case 'code39_extended':
-                $type = BarcodeGenerator::Code39Extended;
-                break;
-            case 'ean128':
-                $type = BarcodeGenerator::Ean128;
-                break;
-            case 'gs1128':
-                $type = BarcodeGenerator::Gs1128;
-                break;
-            case 'i25':
-                $type = BarcodeGenerator::I25;
-                break;
-            case 'isbn':
-                $type = BarcodeGenerator::Isbn;
-                break;
-            case 'msi':
-                $type = BarcodeGenerator::Msi;
-                break;
-            case 'postnet':
-                $type = BarcodeGenerator::Postnet;
-                break;
-            case 's25':
-                $type = BarcodeGenerator::S25;
-                break;
-            case 'upca':
-                $type = BarcodeGenerator::Upca;
-                break;
-            default:
-                $type = BarcodeGenerator::Code128;
-        }
+        return $data;
 
-        $barcode = new BarcodeGenerator();
-        $barcode->setText($data['text']);
-        $barcode->setType($type);
-        $barcode->setScale($data['scale']);
-        $barcode->setThickness($data['thickness']);
-        $barcode->setFontSize($data['font_size']);
-        $barcode->setFormat($data['suffix']);
-
-        $code = $barcode->generate();
-        if ($data['generate'] == 'base64') {
-            return $code;
-        }
-
-//        header('Content-type: image/png');
-        $code = base64_decode($code);
-        print $code;
-        exit();
-//        return $code;
+//        switch ($data['type']) {
+//            case 'codabar':
+//                $type = BarcodeGenerator::Codabar;
+//                break;
+//            case 'code11':
+//                $type = BarcodeGenerator::Code11;
+//                break;
+//            case 'code39':
+//                $type = BarcodeGenerator::Code39;
+//                break;
+//            case 'code39_extended':
+//                $type = BarcodeGenerator::Code39Extended;
+//                break;
+//            case 'ean128':
+//                $type = BarcodeGenerator::Ean128;
+//                break;
+//            case 'gs1128':
+//                $type = BarcodeGenerator::Gs1128;
+//                break;
+//            case 'i25':
+//                $type = BarcodeGenerator::I25;
+//                break;
+//            case 'isbn':
+//                $type = BarcodeGenerator::Isbn;
+//                break;
+//            case 'msi':
+//                $type = BarcodeGenerator::Msi;
+//                break;
+//            case 'postnet':
+//                $type = BarcodeGenerator::Postnet;
+//                break;
+//            case 's25':
+//                $type = BarcodeGenerator::S25;
+//                break;
+//            case 'upca':
+//                $type = BarcodeGenerator::Upca;
+//                break;
+//            default:
+//                $type = BarcodeGenerator::Code128;
+//        }
+//
+//        $barcode = new BarcodeGenerator();
+//        $barcode->setText($data['text']);
+//        $barcode->setType($type);
+//        $barcode->setScale($data['scale']);
+//        $barcode->setThickness($data['thickness']);
+//        $barcode->setFontSize($data['font_size']);
+//        $barcode->setFormat($data['suffix']);
+//
+//        $code = $barcode->generate();
+//        if ($data['generate'] == 'base64') {
+//            return $code;
+//        }
+//
+////        header('Content-type: image/png');
+//        $code = base64_decode($code);
+//        print $code;
+//        exit();
+////        return $code;
     }
 }
