32155c7106b77172df534e8743c59049b628030b
思维空间 <dnyz520@126.com>
2020-05-11 10:03:22 +0800

    install模块准备

---
+++ application/install/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    安装控制器
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2020/5/11
+ */
+
+namespace app\install\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '欢迎使用CareyShop商城框架系统 - Install';
+    }
+}
---
+++ application/install/data/README.md
@@ -0,0 +1,10 @@
+CareyShop
+===============
+
+# 数据库模板导入文件
+
+```
+careyshop.sql       数据库初始化结构及数据
+careyshop_demo.sql  可选择导入演示数据
+cs_region_full.sql  cs_region 表全区域数据(相比较初始化数据会更详细，但同样也会增加资源消耗)
+```
