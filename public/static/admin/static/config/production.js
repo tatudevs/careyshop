877980ce1893c5e6a5d6fd7bb44704603ef1c684
思维空间 <dnyz520@126.com>
2020-05-26 23:38:13 +0800

    后台打包更新

--- public/static/admin/static/config/production.js
+++ public/static/admin/static/config/production.js
@@ -1,5 +1,5 @@
 const serverConfig = {
-  BASE_API: 'http://careyshop.oruei.com/index.php/api',
+  BASE_API: 'http://careyshop.oruei.com/api',
   APP_KEY: '31598984',
   APP_SECRET: '89a9daf553c1abde8afbb0eb1dad2b38'
 }
