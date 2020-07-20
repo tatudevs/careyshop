de9b122b4ed66ecb90162f852c7f617501763de7
思维空间 <dnyz520@126.com>
2020-07-20 15:15:48 +0800

    API配置修改

---
+++ app/api/config/app.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    API应用设置
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2020/7/20
+ */
+
+return [
+    // API调试模式
+    'api_debug'    => env('api_debug', false),
+    // API请求结果为空时返回内容
+    'empty_result' => null,
+];
