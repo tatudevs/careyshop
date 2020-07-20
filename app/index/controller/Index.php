fbaf7d54af8860be47ad2fe725c9b81497feab31
思维空间 <dnyz520@126.com>
2020-07-20 10:01:03 +0800

    no message

---
+++ app/index/controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    商城前台控制器
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2017/03/23
+ */
+
+declare (strict_types=1);
+
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+    }
+}
