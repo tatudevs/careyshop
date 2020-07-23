3cda09d6913369a2f829f8d2fb669964124f38b2
思维空间 <dnyz520@126.com>
2020-07-23 16:56:48 +0800

    App 重构准备

---
+++ app/common/service/App.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    应用管理服务层
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2020/7/23
+ */
+
+namespace app\common\service;
+
+use careyshop\facade\Captcha;
+use think\facade\Route;
+
+class App extends CareyShop
+{
+    /**
+     * 获取应用验证码调用地址
+     * @access public
+     * @return mixed
+     */
+    public function getCaptchaCallurl()
+    {
+        $vars = ['method' => 'image.app.captcha'];
+        $data['call_url'] = Route::buildUrl('/api/v1/app', $vars)->domain(true)->build();
+
+        return $data;
+    }
+
+    /**
+     * 获取应用验证码
+     * @access public
+     * @return mixed
+     */
+    public function imageAppCaptcha()
+    {
+//        $id = input('param.session_id');
+//        $generate = input('param.generate');
+//
+//        $image = Captcha::create(null, true);
+////        $captcha = new Captcha($config);
+////        $image = $captcha->getImage($id);
+//
+//        if ($generate == 'base64') {
+//            return [
+//                'content_type' => 'image/png',
+//                'base64'       => base64_encode($image),
+//            ];
+//        } else {
+//            $result = response($image, 200, ['Content-Length' => strlen($image)])
+//                ->contentType('image/png');
+//
+//            return [
+//                'callback_return_type' => 'response',
+//                'is_callback'          => $result,
+//            ];
+//        }
+        return [];
+    }
+
+    /**
+     * 验证应用验证码
+     * @access public
+     * @param string $code 验证码
+     * @return bool
+     */
+    public static function checkCaptcha($code)
+    {
+//        $captcha = new Captcha();
+//        $id = Request::instance()->param('session_id');
+//
+//        $result = $captcha->check($code, $id);
+//        return $result === false ? $captcha->getError() : true;
+        return true;
+    }
+}
