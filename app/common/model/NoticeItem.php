0b18e976b761ad2582aaba9164e055635625ba48
思维空间 <dnyz520@126.com>
2020-08-03 23:16:16 +0800

    NoticeItem 模型完成

--- app/common/model/NoticeItem.php
+++ app/common/model/NoticeItem.php
@@ -5,13 +5,19 @@
  * CareyShop    通知系统可用变量模型
  *
  * @author      zxm <252404501@qq.com>
- * @date        2017/7/18
+ * @date        2020/8/3
  */
 
 namespace app\common\model;
 
 class NoticeItem extends CareyShop
 {
+    /**
+     * 主键
+     * @var string
+     */
+    protected $pk = 'notice_item_id';
+
     /**
      * 隐藏属性
      * @var array
@@ -30,14 +36,5 @@ class NoticeItem extends CareyShop
         'item_name',
         'replace_name',
         'type',
-    ];
-
-    /**
-     * 字段类型或者格式转换
-     * @var array
-     */
-    protected $type = [
-        'notice_item_id' => 'integer',
-        'type'           => 'integer',
     ];
 }
