1602d86b4503f9049c6e3dfd0c03d3b483de0c76
思维空间 <dnyz520@126.com>
2020-08-03 23:19:50 +0800

    SpecImage 模型完成

---
+++ app/common/model/SpecImage.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    商品规格图片模型
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2020/8/3
+ */
+
+namespace app\common\model;
+
+class SpecImage extends CareyShop
+{
+    /**
+     * 隐藏属性
+     * @var array
+     */
+    protected $hidden = [
+        'goods_id',
+    ];
+
+    /**
+     * 字段类型或者格式转换
+     * @var array
+     */
+    protected $type = [
+        'image' => 'array',
+    ];
+
+    /**
+     * 添加商品规格图片
+     * @access public
+     * @param int   $goodsId 商品编号
+     * @param array $data    外部数据
+     * @return bool
+     * @throws
+     */
+    public function addSpecImage($goodsId, $data)
+    {
+        // 处理部分数据,并进行验证
+        foreach ($data as $key => $value) {
+            $data[$key]['goods_id'] = $goodsId;
+
+            if (!$this->validateData($data[$key])) {
+                return false;
+            }
+        }
+
+        $result = $this->saveAll($data);
+        return false !== $result;
+    }
+}
