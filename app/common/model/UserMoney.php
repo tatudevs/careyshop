6737e052603a1fb4238fe8943136e13c6b8b8237
思维空间 <dnyz520@126.com>
2020-08-03 16:24:04 +0800

    UserMoney 模型完成

--- app/common/model/UserMoney.php
+++ app/common/model/UserMoney.php
@@ -5,13 +5,19 @@
  * CareyShop    账号资金模型
  *
  * @author      zxm <252404501@qq.com>
- * @date        2017/3/30
+ * @date        2020/8/3
  */
 
 namespace app\common\model;
 
 class UserMoney extends CareyShop
 {
+    /**
+     * 主键
+     * @var string
+     */
+    protected $pk = 'user_money_id';
+
     /**
      * 只读属性
      * @var array
@@ -26,20 +32,16 @@ class UserMoney extends CareyShop
      * @var array
      */
     protected $type = [
-        'user_money_id' => 'integer',
-        'user_id'       => 'integer',
-        'total_money'   => 'float',
-        'balance'       => 'float',
-        'lock_balance'  => 'float',
-        'points'        => 'integer',
-        'lock_points'   => 'integer',
+        'total_money'  => 'float',
+        'balance'      => 'float',
+        'lock_balance' => 'float',
     ];
 
     /**
      * 减少可用余额,并增加锁定余额
      * @access public
-     * @param  float $value    数值
-     * @param  int   $clientId 账号编号
+     * @param float $value    数值
+     * @param int   $clientId 账号编号
      * @return bool
      */
     public function decBalanceAndIncLock($value = 0.0, $clientId = 0)
@@ -49,10 +51,10 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         // 查询可用余额是否充足
-        if (bccomp($this->where($map)->value('balance', 0, true), $value, 2) === -1) {
+        if (bccomp($this->where($map)->value('balance', 0), $value, 2) === -1) {
             return $this->setError('账号可用余额不足');
         }
 
@@ -66,8 +68,8 @@ class UserMoney extends CareyShop
     /**
      * 增加可用余额,并减少锁定余额
      * @access public
-     * @param  float $value    数值
-     * @param  int   $clientId 账号编号
+     * @param float $value    数值
+     * @param int   $clientId 账号编号
      * @return bool
      */
     public function incBalanceAndDecLock($value = 0.0, $clientId = 0)
@@ -77,10 +79,10 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         // 查询锁定余额是否充足
-        if (bccomp($this->where($map)->value('lock_balance', 0, true), $value, 2) === -1) {
+        if (bccomp($this->where($map)->value('lock_balance', 0), $value, 2) === -1) {
             return $this->setError('账号锁定余额不足');
         }
 
@@ -94,8 +96,8 @@ class UserMoney extends CareyShop
     /**
      * 减少账号积分,并增加锁定积分
      * @access public
-     * @param  int $value    数值
-     * @param  int $clientId 账号编号
+     * @param int $value    数值
+     * @param int $clientId 账号编号
      * @return bool
      */
     public function decPointsAndIncLock($value = 0, $clientId = 0)
@@ -105,10 +107,10 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         // 查询账号积分是否充足
-        if (bccomp($this->where($map)->value('points', 0, true), $value, 2) === -1) {
+        if (bccomp($this->where($map)->value('points', 0), $value, 2) === -1) {
             return $this->setError('账号可用积分不足');
         }
 
@@ -122,8 +124,8 @@ class UserMoney extends CareyShop
     /**
      * 增加账号积分,并减少锁定积分
      * @access public
-     * @param  int $value    数值
-     * @param  int $clientId 账号编号
+     * @param int $value    数值
+     * @param int $clientId 账号编号
      * @return bool
      */
     public function incPointsAndDecLocl($value = 0, $clientId = 0)
@@ -133,10 +135,10 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         // 查询锁定积分是否充足
-        if (bccomp($this->where($map)->value('lock_points', 0, true), $value, 2) === -1) {
+        if (bccomp($this->where($map)->value('lock_points', 0), $value, 2) === -1) {
             return $this->setError('账号锁定积分不足');
         }
 
@@ -150,8 +152,8 @@ class UserMoney extends CareyShop
     /**
      * 减少锁定余额
      * @access public
-     * @param  float $value    数值
-     * @param  int   $clientId 账号编号
+     * @param float $value    数值
+     * @param int   $clientId 账号编号
      * @return bool
      * @throws
      */
@@ -162,14 +164,14 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         // 查看锁定余额是否充足
-        if (bccomp($this->where($map)->value('lock_balance', 0, true), $value, 2) === -1) {
+        if (bccomp($this->where($map)->value('lock_balance', 0), $value, 2) === -1) {
             return $this->setError('账号锁定余额不足');
         }
 
-        if ($this->where($map)->setDec('lock_balance', $value)) {
+        if ($this->where($map)->dec('lock_balance', $value)->update()) {
             return true;
         }
 
@@ -179,8 +181,8 @@ class UserMoney extends CareyShop
     /**
      * 减少锁定积分
      * @access public
-     * @param  int $value    数值
-     * @param  int $clientId 账号编号
+     * @param int $value    数值
+     * @param int $clientId 账号编号
      * @return bool
      * @throws
      */
@@ -191,14 +193,14 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         // 查询锁定积分是否充足
-        if (bccomp($this->where($map)->value('lock_points', 0, true), $value, 2) === -1) {
+        if (bccomp($this->where($map)->value('lock_points', 0), $value, 2) === -1) {
             return $this->setError('账号锁定积分不足');
         }
 
-        if ($this->where($map)->setDec('lock_points', $value)) {
+        if ($this->where($map)->dec('lock_points', $value)->update()) {
             return true;
         }
 
@@ -208,8 +210,8 @@ class UserMoney extends CareyShop
     /**
      * 增加或减少可用余额
      * @access public
-     * @param  float $value    数值
-     * @param  int   $clientId 账号编号
+     * @param float $value    数值
+     * @param int   $clientId 账号编号
      * @return bool
      * @throws
      */
@@ -220,18 +222,18 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         if ($value > 0) {
-            if ($this->where($map)->setInc('balance', $value)) {
+            if ($this->where($map)->inc('balance', $value)->update()) {
                 return true;
             }
         } else {
-            if (bccomp($this->where($map)->value('balance', 0, true), $value, 2) === -1) {
+            if (bccomp($this->where($map)->value('balance', 0), $value, 2) === -1) {
                 return $this->setError('账号可用余额不足');
             }
 
-            if ($this->where($map)->setDec('balance', -$value)) {
+            if ($this->where($map)->dec('balance', -$value)->update()) {
                 return true;
             }
         }
@@ -242,8 +244,8 @@ class UserMoney extends CareyShop
     /**
      * 增加或减少账号积分
      * @access public
-     * @param  int $value    数值
-     * @param  int $clientId 账号编号
+     * @param int $value    数值
+     * @param int $clientId 账号编号
      * @return bool
      * @throws
      */
@@ -254,18 +256,18 @@ class UserMoney extends CareyShop
         }
 
         // 查询条件
-        $map['user_id'] = ['eq', $clientId];
+        $map[] = ['user_id', '=', $clientId];
 
         if ($value > 0) {
-            if ($this->where($map)->setInc('points', $value)) {
+            if ($this->where($map)->inc('points', $value)->update()) {
                 return true;
             }
         } else {
-            if (bccomp($this->where($map)->value('points', 0, true), $value, 2) === -1) {
+            if (bccomp($this->where($map)->value('points', 0), $value, 2) === -1) {
                 return $this->setError('账号可用积分不足');
             }
 
-            if ($this->where($map)->setDec('points', -$value)) {
+            if ($this->where($map)->dec('points', -$value)->update()) {
                 return true;
             }
         }
@@ -276,8 +278,8 @@ class UserMoney extends CareyShop
     /**
      * 增加账号累计消费金额,并调整账号会员等级
      * @access public
-     * @param  float $value    数值
-     * @param  int   $clientId 账号编号
+     * @param float $value    数值
+     * @param int   $clientId 账号编号
      * @return bool
      * @throws
      */
@@ -288,13 +290,13 @@ class UserMoney extends CareyShop
         }
 
         // 获取当前账号信息
-        $result = $this->where(['user_id' => ['eq', $clientId]])->find();
-        if (!$result) {
-            return is_null($result) ? $this->setError('数据不存在') : false;
+        $result = $this->where(['user_id' => $clientId])->find();
+        if (is_null($result)) {
+            return $this->setError('数据不存在');
         }
 
         // 调整账号累计消费金额,并且重置账号会员等级
-        if ($result->setInc('total_money', $value)) {
+        if ($result->inc('total_money', $value)->update()) {
             $this->setUserLevel($result->getAttr('total_money'), $clientId);
         }
 
@@ -304,8 +306,8 @@ class UserMoney extends CareyShop
     /**
      * 减少账号累计消费金额,并调整账号会员等级
      * @access public
-     * @param  float $value    数值
-     * @param  int   $clientId 账号编号
+     * @param float $value    数值
+     * @param int   $clientId 账号编号
      * @return bool
      * @throws
      */
@@ -316,13 +318,13 @@ class UserMoney extends CareyShop
         }
 
         // 获取当前账号信息
-        $result = $this->where(['user_id' => ['eq', $clientId]])->find();
-        if (!$result) {
-            return is_null($result) ? $this->setError('数据不存在') : false;
+        $result = $this->where(['user_id' => $clientId])->find();
+        if (is_null($result)) {
+            return $this->setError('数据不存在');
         }
 
         // 调整账号累计消费金额,并且重置账号会员等级
-        if ($result->setDec('total_money', $value)) {
+        if ($result->dec('total_money', $value)->update()) {
             $this->setUserLevel($result->getAttr('total_money'), $clientId);
         }
 
@@ -332,14 +334,14 @@ class UserMoney extends CareyShop
     /**
      * 重置账号会员等级
      * @access public
-     * @param  float $totalMoney 累计消费金额
-     * @param  int   $clientId   账号编号
+     * @param float $totalMoney 累计消费金额
+     * @param int   $clientId   账号编号
      * @return void
      * @throws
      */
     private function setUserLevel($totalMoney, $clientId)
     {
-        $result = UserLevel::where(['amount' => ['elt', $totalMoney]])
+        $result = UserLevel::where([['amount', '<=', $totalMoney]])
             ->order(['amount' => 'desc'])
             ->find();
 
@@ -350,32 +352,28 @@ class UserMoney extends CareyShop
         $data['level_icon'] = $result->getAttr('icon');
         $data['user_level_id'] = $result->getAttr('user_level_id');
 
-        User::where(['user_id' => ['eq', $clientId]])->update($data);
+        User::where(['user_id' => $clientId])->update($data);
     }
 
     /**
      * 获取指定账号资金信息
      * @access public
-     * @param  array $data 外部数据
+     * @param array $data 外部数据
      * @return array|false
      * @throws
      */
     public function getUserMoneyInfo($data)
     {
-        if (!$this->validateData($data, 'UserMoney')) {
+        if (!$this->validateData($data)) {
             return false;
         }
 
         // 管理员可选择性查看,用户组必须指定
-        $result = self::get(function ($query) use ($data) {
-            $map['user_id'] = ['eq', is_client_admin() ? $data['client_id'] : get_client_id()];
-            $query->field('total_money,balance,lock_balance,points,lock_points')->where($map);
-        });
+        $map[] = ['user_id', '=', is_client_admin() ? $data['client_id'] : get_client_id()];
 
-        if (false !== $result) {
-            return is_null($result) ? null : $result->toArray();
-        }
+        $field = 'total_money,balance,lock_balance,points,lock_points';
+        $result = $this->where($map)->field($field)->find();
 
-        return false;
+        return is_null($result) ? null : $result->toArray();
     }
 }
