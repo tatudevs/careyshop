d67908a1b8543759d401e959282b810fab960d65
思维空间 <dnyz520@126.com>
2020-08-03 10:19:19 +0800

    Notice 服务层模型完成

---
+++ app/common/service/Notice.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    通知系统服务层
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2020/8/3
+ */
+
+namespace app\common\service;
+
+class Notice extends CareyShop
+{
+    /**
+     * 获取通知系统列表
+     * @access public
+     * @return array|false
+     */
+    public static function getNoticeList()
+    {
+        $result = config('careyshop.notice');
+        foreach ($result as $key => $value) {
+            if (!empty($value['value'])) {
+                $result[$key]['value'] = json_decode($value['value'], true);
+            }
+        }
+
+        return $result;
+    }
+}
