637d7ca776ac808e252770f66bb34624a9aa5cd8
思维空间 <dnyz520@126.com>
2020-09-17 17:11:27 +0800

    更新数据导入模板

--- app/api/controller/v1/Stats.php
+++ app/api/controller/v1/Stats.php
@@ -2,7 +2,7 @@
 /**
  * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
  *
- * CareyShop
+ * CareyShop    数据统计控制器
  *
  * @author      zxm <252404501@qq.com>
  * @date        2020/9/17
@@ -10,8 +10,20 @@
 
 namespace app\api\controller\v1;
 
+use app\api\controller\CareyShop;
 
-class Stats
+class Stats extends CareyShop
 {
-
+    /**
+     * 方法路由器
+     * @access protected
+     * @return array
+     */
+    protected static function initMethod()
+    {
+        return [
+            // 获取后台首页统计数据
+            'get.stats.index' => ['getStatsIndex'],
+        ];
+    }
 }
