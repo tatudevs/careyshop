377a469176cbcc06c44af4be7510a6c63799fa22
思维空间 <dnyz520@126.com>
2020-09-18 15:45:13 +0800

    数据统计控制器

--- app/api/controller/v1/Stats.php
+++ app/api/controller/v1/Stats.php
@@ -12,6 +12,8 @@ namespace app\api\controller\v1;
 
 use app\api\controller\CareyShop;
 use app\common\service\Stats as StatsService;
+use careyshop\Time;
+use think\exception\ValidateException;
 
 class Stats extends CareyShop
 {
@@ -23,16 +25,50 @@ class Stats extends CareyShop
     protected static function initMethod()
     {
         return [
-            // 获取后台首页统计数据
-            'get.stats.index'  => ['getStatsIndex', StatsService::class],
-            // 获取店铺统计数据
-            'get.stats.shop'   => ['getStatsShop', StatsService::class],
-            // 获取商品统计数据
-            'get.stats.goods'  => ['getStatsGoods', StatsService::class],
-            // 获取订单统计数据
-            'get.stats.order'  => ['getStatsOrder', StatsService::class],
-            // 获取会员统计数据
-            'get.stats.client' => ['getStatsClient', StatsService::class],
+            // 获取某一项数据统计
+            'get.stats.data' => ['getStatsData', false],
         ];
+    }
+
+    /**
+     * 获取某一项数据统计
+     * @access protected
+     * @return array|false
+     */
+    protected function getStatsData()
+    {
+        // 外部数据验证
+        try {
+            $data = $this->getParams();
+            $this->validate($data, \app\common\validate\Stats::class);
+        } catch (ValidateException $e) {
+            return $this->setError($e->getMessage());
+        }
+
+        // 默认取最近7天
+        if (empty($data['begin_time']) || empty($data['end_time'])) {
+            [$data['begin_time'], $data['end_time']] = Time::dayToNow(7);
+        }
+
+        $result = [];
+        switch ($data['type']) {
+            case 'index':
+                $result = StatsService::getStatsIndex();
+                break;
+            case 'shop':
+                $result = StatsService::getStatsShop($data['begin_time'], $data['end_time']);
+                break;
+            case 'goods':
+                $result = StatsService::getStatsGoods($data['begin_time'], $data['end_time']);
+                break;
+            case 'order':
+                $result = StatsService::getStatsOrder($data['begin_time'], $data['end_time']);
+                break;
+            case 'client':
+                $result = StatsService::getStatsClient($data['begin_time'], $data['end_time']);
+                break;
+        }
+
+        return $result;
     }
 }
