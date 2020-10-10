0ce9dfc6534094ba1054a2ac6b375c24b93d3a1d
思维空间 <dnyz520@126.com>
2020-10-10 14:52:16 +0800

    修改注释内容

--- app/service.php
+++ app/service.php
@@ -2,7 +2,7 @@
 /**
  * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
  *
- * CareyShop    系统服务定义文件
+ * CareyShop    全局系统服务定义文件
  *
  * @author      zxm <252404501@qq.com>
  * @date        2020/7/20
