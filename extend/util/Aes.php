4c4b14942d88fad1d0e9b58c4d27a5d19b1376e2
思维空间 <dnyz520@126.com>
2021-01-12 17:04:19 +0800

    AES

---
+++ extend/util/Aes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    AES
+ *
+ * @author      zxm <252404501@qq.com>
+ * @date        2021/1/12
+ */
+
+namespace util;
+
+class Aes
+{
+}
