ee58d08173c21bc38202283fb7cd66e528a62fad
思维空间 <dnyz520@126.com>
2021-01-15 09:29:57 +0800

    恢复版本号

--- config/extra.php
+++ config/extra.php
@@ -12,7 +12,7 @@ return [
     // 产品信息
     'product'      => [
         'product_name'    => 'CareyShop商城框架系统',
-        'product_version' => '1.3.3',
+        'product_version' => '1.3.2',
         'build_version'   => '20210111',
         'product_website' => 'http://www.careyshop.cn',
         'product_update'  => 'http://www.careyshop.cn/checkUpdate',
