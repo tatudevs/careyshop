17d3db08ef4b76a009321e717d44d927ae71dacd
思维空间 <dnyz520@126.com>
2021-04-09 01:47:17 +0800

    代码片段优化

--- app/common.php
+++ app/common.php
@@ -188,20 +188,26 @@ if (!function_exists('guid_v4')) {
             return $trim === true ? trim(com_create_guid(), '{}') : com_create_guid();
         }
 
+        // trim
+        $lbrace = $trim ? '' : chr(123); // "{"
+        $rbrace = $trim ? '' : chr(125); // "}"
+
         // OSX/Linux
         if (function_exists('openssl_random_pseudo_bytes')) {
             $data = openssl_random_pseudo_bytes(16);
             $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
             $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
-            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+
+            return $lbrace .
+                vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4)) .
+                $rbrace;
         }
 
         // Fallback(PHP 4.2+)
         mt_srand((double)microtime() * 10000);
         $charid = strtolower(md5(uniqid(rand(), true)));
-        $hyphen = chr(45);                  // "-"
-        $lbrace = $trim ? '' : chr(123);    // "{"
-        $rbrace = $trim ? '' : chr(125);    // "}"
+        $hyphen = chr(45); // "-"
+
         return $lbrace .
             substr($charid, 0, 8) . $hyphen .
             substr($charid, 8, 4) . $hyphen .
