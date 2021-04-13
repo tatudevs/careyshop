b73fd027fed0727645ea1499bc8c1350fef2d0f6
思维空间 <dnyz520@126.com>
2021-04-13 10:30:38 +0800

    增加"redis"配置

--- config/cache.php
+++ config/cache.php
@@ -14,6 +14,7 @@ return [
 
     // 缓存连接方式配置
     'stores'  => [
+        // 框架默认缓存
         'file'  => [
             // 驱动方式
             'type'       => 'File',
@@ -28,6 +29,7 @@ return [
             // 序列化机制 例如 ['serialize', 'unserialize']
             'serialize'  => [],
         ],
+        // 第三方本地数据(如WeChat,可根据需要修改驱动方式)
         'place' => [
             // 驱动方式(需要数据持久化)
             'type'       => 'File',
@@ -40,6 +42,31 @@ return [
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
+            'serialize'  => [],
+        ],
+        // Redis缓存
+        'redis' => [
+            // 驱动方式
+            'type'       => 'Redis',
+            // 服务器地址
+            'host'       => '127.0.0.1',
+            // 端口
+            'port'       => 6379,
+            // 密码
+            'password'   => '',
+            // 数据库索引号
+            'select'     => 0,
+            // 连接超时
+            'timeout'    => 0,
+            // 缓存有效期 0表示永久缓存
+            'expire'     => 0,
+            // 是否持久化
+            'persistent' => false,
+            // 缓存前缀
+            'prefix'     => '',
+            // 缓存标签前缀
+            'tag_prefix' => 'tag:',
+            // 序列化机制
             'serialize'  => [],
         ]
         // 更多的缓存连接
