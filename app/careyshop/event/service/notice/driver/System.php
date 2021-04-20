d18a55af25851bbd8eea142eeed6ea8d8054cb95
思维空间 <dnyz520@126.com>
2021-04-21 02:15:45 +0800

    通知系统

---
+++ app/careyshop/event/service/notice/driver/System.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright   Copyright (c) http://careyshop.cn All rights reserved.
+ *
+ * CareyShop    系统通知驱动
+ *
+ * @author      zxm <252404501@qq.com>
+ * @version     v1.1
+ * @date        2021/4/20
+ */
+
+namespace app\careyshop\event\service\notice\driver;
+
+use app\careyshop\event\service\notice\Driver;
+
+class System extends Driver
+{
+}
